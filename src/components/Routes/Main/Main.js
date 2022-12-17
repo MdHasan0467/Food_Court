55d4825879e1f12bc9352ec7f988383fc5c41051
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-12-18 02:23:07 +0600

    Main route & navbar basic setup

--- src/components/Routes/Main/Main.js
+++ src/components/Routes/Main/Main.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import LargeNavBar from '../../Shared/NavBar/LargeNavBar/LargeNavBar';
+import SmallNavBar from '../../Shared/NavBar/SmallNavBar/SmallNavBar';
 
 const Main = () => {
     return (
-        <div>
+			<div>
+				<div className='hidden md:block'>
+					<LargeNavBar></LargeNavBar>
+					<Outlet></Outlet>
+				</div>
+            <div className=' md:hidden'>
             <Outlet></Outlet>
-        </div>
-    );
+            <SmallNavBar></SmallNavBar>
+				</div>
+			</div>
+		);
 };
 
 export default Main;
---
+++ src/components/Shared/NavBar/SmallNavBar/SmallNavBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const SmallNavBar = () => {
+    return (
+			<div>
+				<div className='bg-base-100 flex justify-center'>
+					<ul className='flex'>
+						<li>
+							<a>Item 1</a>
+						</li>
+						<li>
+							<a>Item 1</a>
+						</li>
+						<li>
+							<a>Item 1</a>
+						</li>
+						<li>
+							<a>Item 1</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		);
+};
+
+export default SmallNavBar;
