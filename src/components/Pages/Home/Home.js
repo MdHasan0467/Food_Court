76d3c16a329814a382cf7405d407c72201fb15b2
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-12-18 03:10:26 +0600

    bgBanner added & navbar setup

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import Banner from './Banner/Banner';
+import LargeNavBar from '../../Shared/NavBar/LargeNavBar/LargeNavBar';
 
 const Home = () => {
     return (
 			<div>
-				<Banner></Banner>
+				<div className='bgBanner min-h-[50vh]'>
+					<div className='hidden md:block'>
+						<LargeNavBar></LargeNavBar>
+					</div>
+					<div className='grid grid-cols-1 md:grid-cols-2'>
+						<div className='content'>
+							<div className='text-white blurEffect text-start '>
+								<h2 className='text-start ml-[20%] text-3xl'>Welcome To Our</h2>
+								<h2 className='text-start ml-[20%] text-4xl'>Food Court Restaurant</h2>
+							</div>
+						</div>
+						<div className=''></div>
+					</div>
+				</div>
 			</div>
 		);
 };
--- src/components/Routes/Main/Main.js
+++ src/components/Routes/Main/Main.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import LargeNavBar from '../../Shared/NavBar/LargeNavBar/LargeNavBar';
-import SmallNavBar from '../../Shared/NavBar/SmallNavBar/SmallNavBar';
 
 const Main = () => {
     return (
 			<div>
-				<div className='hidden md:block'>
-					<LargeNavBar></LargeNavBar>
 					<Outlet></Outlet>
-				</div>
-            <div className=' md:hidden'>
-            <Outlet></Outlet>
-            <SmallNavBar></SmallNavBar>
-				</div>
 			</div>
 		);
 };
