a79ce2e470ad2b09e2d4908f29c4096819a125e9
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-12-18 03:33:08 +0600

    bgBanner content added

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -9,10 +9,36 @@ const Home = () => {
 						<LargeNavBar></LargeNavBar>
 					</div>
 					<div className='grid grid-cols-1 md:grid-cols-2'>
-						<div className='content'>
+						<div className='content py-5'>
 							<div className='text-white blurEffect text-start '>
-								<h2 className='text-start ml-[20%] text-3xl'>Welcome To Our</h2>
-								<h2 className='text-start ml-[20%] text-4xl'>Food Court Restaurant</h2>
+								<div className='heading-content'>
+									<h2 className='text-start ml-[20%] text-3xl'>
+										Welcome To Our
+									</h2>
+									<h2 className='text-start ml-[20%] text-4xl'>
+										Food Court Restaurant
+									</h2>
+								</div>
+
+								<div className='text-content mt-7'>
+									<h1 className='text-start ml-[20%] flex'>
+										<h3 className='text-4xl'>F</h3>
+										<h2 className='mt-1'>
+											<h5 className='ml-1'>
+												ood is a major part of basic human needs.
+											</h5>
+											<h5>
+												Everyone loves to eat. And there is no such thing as a
+												food lover.
+											</h5>
+										</h2>
+									</h1>
+									<h1 className='text-start ml-[20%] mt-3'>
+										If you are a food lover and want to eat different items,
+										then Food Court is offering you both Bengali and
+										international most popular food items.
+									</h1>
+								</div>
 							</div>
 						</div>
 						<div className=''></div>
--- src/components/Shared/NavBar/LargeNavBar/LargeNavBar.js
+++ src/components/Shared/NavBar/LargeNavBar/LargeNavBar.js
@@ -2,23 +2,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const LargeNavBar = () => {
-    return (
-			<div>
-				<div className='navbar bg-transparent text-white'>
-					<div className='navbar-start'>
-						<a className='btn btn-ghost normal-case text-xl'>daisyUI</a>
-					</div>
-					<div className='navbar-center hidden lg:flex'>
-						<Link className='ml-5' to='/'>Home</Link>
-						<Link className='ml-5' to='/addproduct'>Add Product</Link>
-						<Link className='ml-5' to='/about'>About</Link>
-					</div>
-					<div className='navbar-end'>
-						<a className='btn'>Login</a>
-					</div>
+	return (
+		<div>
+			<div className='navbar bg-transparent text-white'>
+				<div className='navbar-start'>
+					<a className='btn btn-ghost normal-case text-xl'>
+						<Link className='nav' to='/'>
+							Food Court
+						</Link>
+					</a>
+				</div>
+				<div className='navbar-center hidden lg:flex'>
+					<Link className='ml-5 nav' to='/'>
+						Home
+					</Link>
+					<Link className='ml-5 nav' to='/addproduct'>
+						Add Product
+					</Link>
+					<Link className='ml-5 nav' to='/about'>
+						About
+					</Link>
+				</div>
+				<div className='navbar-end'>
+					<a className='btn'>Login</a>
 				</div>
 			</div>
-		);
+		</div>
+	);
 };
 
 export default LargeNavBar;
